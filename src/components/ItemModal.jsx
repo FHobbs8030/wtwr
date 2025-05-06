7105f788fb01d3b44ec60fcf51b3f5d444bc75da
FHobbs8030 <FHobbs8030@Gmail.com>
2025-05-06 03:06:58 -0700

    modify and new file

---
+++ src/components/ItemModal.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import "../blocks/ItemModal.css";
+
+function ItemModal({ item, onClose }) {
+  useEffect(() => {
+    const handleEscClose = (e) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("keydown", handleEscClose);
+    return () => document.removeEventListener("keydown", handleEscClose);
+  }, [onClose]);
+
+  const handleOverlayClick = (e) => {
+    if (e.target.classList.contains("modal")) {
+      onClose();
+    }
+  };
+
+  return (
+    <div className="modal" onClick={handleOverlayClick}>
+      <div className="modal__content modal__content_type_image">
+        <button
+          className="modal__close"
+          type="button"
+          onClick={onClose}
+        ></button>
+        <img src={item.imageUrl} alt={item.name} className="modal__image" />
+        <p className="modal__caption">{item.name}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ItemModal;
