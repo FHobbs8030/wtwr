010087c2b374ced5dbed4cd4e01ec3994fd2b606
FHobbs8030 <FHobbs8030@Gmail.com>
2025-05-17 14:19:19 -0700

    modify, new file and rename

--- src/components/ItemModal.jsx
+++ src/components/ItemModal.jsx
@@ -14,23 +14,23 @@ function ItemModal({ item, onClose }) {
   }, [onClose]);
 
   const handleOverlayClick = (e) => {
-    if (e.target.classList.contains("modal")) {
+    if (e.target.classList.contains("item-modal")) {
       onClose();
     }
   };
 
   return (
-    <div className="modal" onClick={handleOverlayClick}>
-      <div className="modal__content modal__content_type_image">
+    <div className="item-modal" onClick={handleOverlayClick}>
+      <div className="item-modal__content item-modal__content_type_image">
         <button
-          className="modal__close"
+          className="item-modal__close"
           type="button"
           onClick={onClose}
         ></button>
-        <img src={item.link} alt={item.name} className="modal__image" />
-        <div className="modal__info">
-          <h2 className="modal__title">{item.name}</h2>
-          <p className="modal__weather">Weather type: {item.weather}</p>
+        <img src={item.link} alt={item.name} className="item-modal__image" />
+        <div className="item-modal__info">
+          <h2 className="item-modal__title">{item.name}</h2>
+          <p className="item-modal__weather">Weather: {item.weather}</p>
         </div>
       </div>
     </div>
