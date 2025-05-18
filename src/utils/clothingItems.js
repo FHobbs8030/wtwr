8678509edd094363c3dab85bb47f850d8795ded5
FHobbs8030 <FHobbs8030@Gmail.com>
2025-05-17 17:25:57 -0700

    modify

--- src/utils/clothingItems.js
+++ src/utils/clothingItems.js
@@ -14,41 +14,34 @@ export const defaultClothingItems = [
 
   {
     _id: 2,
-    name: "Jacket",
-    weather: "cold",
-    link: "/images/jacket1.jpg",
-  },
-
-  {
-    _id: 3,
-    name: "Cap",
-    weather: "warm",
-    link: "/images/cap1.jpg",
-  },
-
-  {
-    _id: 4,
     name: "Sneakers",
     weather: "cold",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Sneakers.png?etag=3efeec41c1c78b8afe26859ca7fa7b6f",
   },
   {
-    _id: 5,
+    _id: 3,
     name: "T-Shirt",
     weather: "hot",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/T-Shirt.png?etag=44ed1963c44ab19cd2f5011522c5fc09",
   },
   {
-    _id: 6,
+    _id: 4,
     name: "Coat",
     weather: "cold",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
   },
 
   {
-    _id: 7,
+    _id: 5,
     name: "Jacket",
     weather: "cold",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Jacket.png?etag=f4bb188deaa25ac84ce2338be2d404ad",
+  },
+
+  {
+    _id: 6,
+    name: "Cap",
+    weather: "warm",
+    link: "/images/cap1.jpg",
   },
 ];
