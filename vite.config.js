63767cac83812a5e26499f316d21cd47b6a7a068
Fred Hobbs <FHobbs8030@Gmail.com>
2025-05-27 09:31:22 -0700

    modify

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/se_project_react/',
   plugins: [react()],
   server: {
     port: 3000,
   },
   resolve: {
     alias: {
-      "~": "/src", // optional alias you can use later
+      '~': '/src', 
     },
   },
 });
-
