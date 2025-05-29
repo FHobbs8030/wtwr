f2f7a5410c06d6f95660bc0440a0792ac83e76a6
Fred Hobbs <FHobbs8030@Gmail.com>
2025-05-28 19:26:57 -0700

    modify

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -3,7 +3,13 @@ import "../blocks/ItemCard.css";
 
 function ItemCard({ item, onCardClick }) {
   return (
-    <div className="card" onClick={() => onCardClick(item)}>
+    <div
+      className="card"
+      onClick={() => {
+        console.log("Card clicked!", item.name);
+        onCardClick(item);
+      }}
+    >
       <div className="card__name">{item.name}</div>
       <div className="card__image-container">
         <img
