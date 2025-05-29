e72950f6c0a249725bcf24cbb6dcc7cd9c7f5db8
Fred Hobbs <FHobbs8030@Gmail.com>
2025-05-28 18:37:28 -0700

    change line 5

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Use '/' for local dev; change to '/se_project_react/' for GitHub Pages
+  base: '/se_project_react/',  
   plugins: [react()],
   server: {
     port: 3000,
